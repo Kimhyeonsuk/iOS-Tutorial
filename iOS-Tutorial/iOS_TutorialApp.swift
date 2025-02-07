bff2f5b78457fdb8010940bfdea8a5701a76abbd
HYEONSUK KIM <sukrrard97@gmail.com>
2025-02-07 14:44:20 +0900

    init

---
+++ iOS-Tutorial/iOS_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_TutorialApp.swift
+//  iOS-Tutorial
+//
+//  Created by 김현석 on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS-TutorialTests/iOS_TutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_TutorialTests.swift
+//  iOS-TutorialTests
+//
+//  Created by 김현석 on 1/30/25.
+//
+
+import Testing
+@testable import iOS_Tutorial
+
+struct iOS_TutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
